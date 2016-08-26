844d140735fb6e84d60b24a03be3b2e65238c29b
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-08-27 02:54:14 +1200

    init files added

---
+++ Abies/settings/production.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 Akshay Raj Gollahalli
+
+from django.conf import settings
+
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+DATABASES = settings.DATABASES
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
