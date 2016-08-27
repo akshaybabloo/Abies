75262a11e8d49abbae1491f280dcd3dbb030f41e
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-08-27 20:16:13 +1200

    ini removed; secrets.py added

--- Abies/settings/base.py
+++ Abies/settings/base.py
@@ -11,20 +11,16 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from configparser import RawConfigParser
-
-config = RawConfigParser()
-config.read('settings.ini')
+from . secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('secrets', 'SECRET_KEY')
+# SECRET_KEY = In secrets.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
