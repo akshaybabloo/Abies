3575685988536497841384e4211d7115df3d7594
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-08-27 22:20:44 +1200

    views app created

--- Abies/urls.py
+++ Abies/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^editor/', include('editor.urls')),
+    url(r'^viewer/', include('viewer.urls')),
     url(r'^admin/', admin.site.urls),
 ]
