b993ec3051004f4e7ce7241cd8d3726893319743
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-08-28 01:03:01 +1200

    editor models and migrations added

--- editor/models.py
+++ editor/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Article(models.Model):
+    article = models.TextField()
+    date = models.DateTimeField('Date Published', default= timezone.now, blank=True)
+    tags = models.CharField(max_length=500)
+    makePrivate = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.article
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
