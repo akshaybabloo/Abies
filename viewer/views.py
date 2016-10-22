207a61fc1c217cd0a95fbaeb21a69735c09fa9f8
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-10-22 17:01:26 +1300

    import changes
    instead of directly importing Abis.settings now importing django.conf
    settings

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.staticfiles.templatetags.staticfiles import static
-from Abies.settings import SHARE_URL
+from django.conf import settings
 import requests
 
 
@@ -13,16 +13,16 @@ def index(request):
 def page_not_found(request):
     url = request.get_full_path()
     img = static('img/404.png')
-    home = SHARE_URL
+    home = settings.SHARE_URL
     template = "error.html"
-    context = {"url": SHARE_URL + url, "error": 404, "error_text": "Page not found.", "img": img, "home": home}
+    context = {"url": home + url, "error": 404, "error_text": "Page not found.", "img": img, "home": home}
     return render(request, template, context)
 
 
 def server_error(request):
     url = request.get_full_path()
-    home = SHARE_URL
+    home = settings.SHARE_URL
     img = static('img/500.png')
     template = "error.html"
-    context = {"url": SHARE_URL + url, "error": 500, "error_text": "Sorry, but the requested page is unavailable due to a server hiccup.", "img": img, "home": home}
+    context = {"url": home + url, "error": 500, "error_text": "Sorry, but the requested page is unavailable due to a server hiccup.", "img": img, "home": home}
     return render(request, template, context)
