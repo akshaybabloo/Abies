aae437b6b1a20304b26f04da10cbab4fac23baa8
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-11-24 19:09:55 +1300

    requests removed

--- settings/views.py
+++ settings/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 from django.http import HttpResponse
-import requests
-
 
 def index(request):
     return HttpResponse("Coming soon")
